c0a21d5ffcb34924c2c0d894e70f1e2e0ab826ee
Ipaye Babatunde <fineboyhakim@gmail.com>
2024-11-17 05:16:38 +0100

    init commit

---
+++ src/main/java/com/ipaye/employeemanagementsystemproject/Model/Role.java
@@ -0,0 +1,21 @@
+package com.ipaye.employeemanagementsystemproject.Model;
+
+import javax.imageio.event.IIOReadProgressListener;
+
+public class Role {
+
+    String roleName;
+
+    public Role(String roleName){
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if(this == object) return true;
+        if(object == null || getClass() != object.getClass()) return false;
+        Role role = (Role) object;
+        return roleName.equals(role.roleName);
+    }
+
+}
